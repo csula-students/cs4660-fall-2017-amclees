a0aa121ac004731456ca081b589797ea489bce28
Andrew McLees <andrewmclees5@gmail.com>
2017-09-02 09:36:29 -0700

    Finish list functions

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,22 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    pass
+    return li[0]
 
 def get_last_item(li):
     """Return the last item from the list"""
-    pass
+    return li[-1]
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    pass
+    if len(li) < 3:
+      raise ValueError('li must have at least 3 items')
+    return [li[1], li[2]]
 
 def get_sum(li):
     """Return the sum of the list items"""
-    pass
+    return reduce((lambda a, b: a + b), li)
 
 def get_avg(li):
     """Returns the average of the list items"""
-    pass
+    return get_sum(li) / len(li) 
