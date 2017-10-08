9f902cf8a4e4f8b5f0dbf9a50aef6491f25721e0
Andrew McLees <andrewmclees5@gmail.com>
2017-10-08 14:10:41 -0700

    Begin on parsing the grid for A*

--- cs4660/graph/utils.py
+++ cs4660/graph/utils.py
@@ -4,6 +4,8 @@ utils package is for some quick utility methods
 such as parsing
 """
 
+import graph as g
+
 class Tile(object):
     """Node represents basic unit of graph"""
     def __init__(self, x, y, symbol):
@@ -26,7 +28,20 @@ class Tile(object):
     def __hash__(self):
         return hash(str(self.x) + "," + str(self.y) + self.symbol)
 
+def block_at_position(lines, x, y):
+    if y >= len(lines) - 1 or x >= len(lines[0]) - 1 or x <= 1 or y <= 1:
+        return '--'
+    return lines[y][x] + lines[y][x + 1]
 
+def edges_at_position(lines, x, y, tag):
+    positions = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+    edges = []
+    for position in positions:
+        block = block_at_position(lines, position[0], position[1])
+        if block == '--':
+            continue
+        edges.append(g.Edge(g.Node(Tile(x, y, tag)), g.Node(Tile(position[0], position[1], block)), 1))
+    return edges
 
 def parse_grid_file(graph, file_path):
     """
@@ -35,9 +50,30 @@ def parse_grid_file(graph, file_path):
 
     Returns graph object
     """
-    # TODO: read the filepaht line by line to construct nodes & edges
+    file_obj = open(file_path)
+    lines = []
+    for line in file_obj:
+        lines.append(line)
 
-    # TODO: for each node/edge above, add it to graph
+    tiles = []
+    edges = []
+    y = 0
+    for line in lines:
+        if line[0] == '+':
+            continue
+        x = 1
+        while x < len(line):
+            block = line[x] + line[x + 1]
+            if block == '  ' or block[0] == '@':
+                tiles.append(g.Node(Tile(x, y, block)))
+                edges += edges_at_position(lines, x, y, block)
+            x += 2
+        y += 1
+
+    for tile in tiles:
+        graph.add_node(tile)
+    for edge in edges:
+        graph.add_edge(edge)
 
     return graph
 
@@ -47,4 +83,19 @@ def convert_edge_to_grid_actions(edges):
 
     e.g. Edge(Node(Tile(1, 2), Tile(2, 2), 1)) => "S"
     """
-    return ""
+    if not edges:
+        return ''
+
+    actions = []
+    for edge in edges:
+        if edge.from_node.x > edge.to_node.x:
+            actions.append('S')
+        elif edge.from_node.x < edge.to_node.x:
+            actions.append('N')
+        elif edge.from_node.y > edge.to_node.y:
+            actions.append('E')
+        else:
+            actions.append('W')
+
+    return ''.join(actions)
+
